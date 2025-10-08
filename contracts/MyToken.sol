3830f0f4bc0a690bc32452de8b237c461e3fc405
3GID <km020207@naver.com>
2025-10-08 17:33:48 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,11 +9,11 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf; 
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount ) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        _mint(1*10**uint256(decimals), msg.sender); // 1 MT에서 추가발행불가
+        _mint(_amount*10**uint256(decimals), msg.sender); // 1 MT에서 추가발행불가
         
     }
     
@@ -23,6 +23,14 @@ contract MyToken {
         // balanceOf[owner] = balanceOf[owner] + amount;
         totalSupply += amount;
         balanceOf[owner] += amount;
+    }
+
+    function transfer(uint256 amount, address to) external{
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
     }
 
     // function totalSupply() external view returns (uint256){
