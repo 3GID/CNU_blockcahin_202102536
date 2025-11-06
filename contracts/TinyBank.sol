4d81a729ba9996a6a07f2aec66914e8893539846
3GID <km020207@naver.com>
2025-11-06 17:33:13 +0900

    add withdraw

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -7,7 +7,8 @@ interface IMyToken {
 }
 
 contract TinyBank {
-    event Staked(address, uint256);
+    event Staked(address from, uint256 amount);
+    event Withdrawn(uint256 amount, address to);
 
     IMyToken public stakingToken;
     mapping(address => uint256) public staked;
@@ -23,5 +24,12 @@ contract TinyBank {
         staked[msg.sender] += _amount;
         totalStaked += _amount;
         emit Staked(msg.sender, _amount);
+    }
+    function withdraw(uint256 _amount) external {
+        require(staked[msg.sender] >= _amount, "insufficient staked tokens");
+        stakingToken.transfer(_amount, msg.sender);
+        staked[msg.sender] -= _amount;
+        totalStaked -= _amount;
+        emit Withdrawn(_amount, msg.sender);
     }
 }
