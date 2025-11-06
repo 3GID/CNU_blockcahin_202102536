b71ba094430264ee98541ce968ffa8e1d28a64c8
3GID <km020207@naver.com>
2025-11-06 17:54:21 +0900

    add reward

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -4,28 +4,43 @@ interface IMyToken {
     function transfer(uint256 amount, address to) external;
 
     function transferFrom(address from, address to, uint256 amount) external;
-}
+    function mint(uint256 amount, address owner) external;
+}  
 
 contract TinyBank {
     event Staked(address from, uint256 amount);
     event Withdrawn(uint256 amount, address to);
 
     IMyToken public stakingToken;
+
+    mapping(address => uint256) public lastClaimedBlock;
+    uint256 rewardPerBlock = 1 * 10 ** 18;
+
     mapping(address => uint256) public staked;
     uint256 public totalStaked;
 
     constructor(IMyToken _stakingToken) {
         stakingToken = _stakingToken;
     }
+    modifier updateReward(address to) {
+        if (staked[to] > 0) {
+            uint256 blocks = block.number - lastClaimedBlock[to];
+            uint256 reward = (blocks * rewardPerBlock * staked[to]) / totalStaked;
+            stakingToken.mint(reward, to);
+        }
+        lastClaimedBlock[to] = block.number;
+        _; 
+    }
 
-    function stake(uint256 _amount) external {
+
+    function stake(uint256 _amount) external updateReward(msg.sender) {
         require(_amount > 0, "cannot stake 0");
         stakingToken.transferFrom(msg.sender, address(this), _amount);
         staked[msg.sender] += _amount;
         totalStaked += _amount;
         emit Staked(msg.sender, _amount);
     }
-    function withdraw(uint256 _amount) external {
+    function withdraw(uint256 _amount) external updateReward(msg.sender) {
         require(staked[msg.sender] >= _amount, "insufficient staked tokens");
         stakingToken.transfer(_amount, msg.sender);
         staked[msg.sender] -= _amount;
--- test/TinyBank.ts
+++ test/TinyBank.ts
@@ -41,7 +41,7 @@ describe("TinyBank", () => {
             expect(await myTokenC.balanceOf(tinyBankC)).equal(await tinyBankC.totalStaked());
         });
     });
-    
+
     describe("Withdraw", () => {
         it("should return 0 staked after withdrawing total token", async () => {
             const signer0 = signers[0];
@@ -50,6 +50,25 @@ describe("TinyBank", () => {
             await tinyBankC.stake(stakingAmount);
             await tinyBankC.withdraw(stakingAmount);
             expect(await tinyBankC.staked(signer0.address)).equal(0);
+        });
+    });
+    describe("reward", () => {
+        it("should reward 1MT every block", async () => {
+            const singer0 = signers[0];
+            const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
+            await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
+            await tinyBankC.stake(stakingAmount);
+
+            const BLOCKS = 5n;
+            const transferAmount = hre.ethers.parseUnits("1", DECIMALS);
+            for (var i = 0; i < BLOCKS; i++) {
+                await myTokenC.transfer(transferAmount, singer0.address);
+            }
+
+            await tinyBankC.withdraw(stakingAmount);
+            expect(await myTokenC.balanceOf(singer0.address)).equal(
+                hre.ethers.parseUnits((BLOCKS + MINTING_AMOUNT + 1n).toString())
+            );
         });
     });
 });
