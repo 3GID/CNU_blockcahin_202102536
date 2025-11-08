151f46d311f93868eac7b14518ca52949b643fc1
3GID <km020207@naver.com>
2025-11-08 21:17:32 +0900

    MultiManagedAccess

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -1,31 +1,32 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "./ManagedAccess.sol";
+import "./MultiManagedAccess.sol";
 
 interface IMyToken {
     function transfer(uint256 amount, address to) external;
-
     function transferFrom(address from, address to, uint256 amount) external;
     function mint(uint256 amount, address owner) external;
-}  
+}
 
-contract TinyBank is ManagedAccess{
+contract TinyBank is MultiManagedAccess {
     event Staked(address from, uint256 amount);
-    event Withdrawn(uint256 amount, address to);
+    event Withdraw(uint256 amount, address to);
 
     IMyToken public stakingToken;
 
     mapping(address => uint256) public lastClaimedBlock;
-    uint256 defaultRewardPerBlock = 1 * 10 ** 18;
-    uint256 rewardPerBlock;
-
+    uint256 public defaultRewardPerBlock = 1 * 10 ** 18;
     mapping(address => uint256) public staked;
     uint256 public totalStaked;
 
-    constructor(IMyToken _stakingToken) ManagedAccess(msg.sender, msg.sender) {
+    constructor(IMyToken _stakingToken, address[] memory _managers)
+        MultiManagedAccess(msg.sender, _managers, _managers.length)
+    {
         stakingToken = _stakingToken;
         rewardPerBlock = defaultRewardPerBlock;
     }
+
     modifier updateReward(address to) {
         if (staked[to] > 0) {
             uint256 blocks = block.number - lastClaimedBlock[to];
@@ -33,26 +34,30 @@ contract TinyBank is ManagedAccess{
             stakingToken.mint(reward, to);
         }
         lastClaimedBlock[to] = block.number;
-        _; 
+        _;
     }
-    function setRewardPerBlock(uint256 _amount) external onlyManager {
-        rewardPerBlock = _amount;
-    }
-
-
 
     function stake(uint256 _amount) external updateReward(msg.sender) {
-        require(_amount > 0, "cannot stake 0");
+        require(_amount > 0, "cannot stake 0 amount");
         stakingToken.transferFrom(msg.sender, address(this), _amount);
         staked[msg.sender] += _amount;
         totalStaked += _amount;
         emit Staked(msg.sender, _amount);
     }
+
     function withdraw(uint256 _amount) external updateReward(msg.sender) {
-        require(staked[msg.sender] >= _amount, "insufficient staked tokens");
+        require(staked[msg.sender] >= _amount, "insufficient staked token");
         stakingToken.transfer(_amount, msg.sender);
         staked[msg.sender] -= _amount;
         totalStaked -= _amount;
-        emit Withdrawn(_amount, msg.sender);
+        emit Withdraw(_amount, msg.sender);
+    }
+
+    function confirm() public override {
+        super.confirm();
+    }
+
+    function setRewardPerBlock(uint256 _amount) public override onlyAllConfirmed {
+        rewardPerBlock = _amount;
     }
 }
--- test/TinyBank.ts
+++ test/TinyBank.ts
@@ -5,78 +5,48 @@ import { MyToken, TinyBank } from "../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("TinyBank", () => {
-    let signers: HardhatEthersSigner[];
-    let myTokenC: MyToken;
-    let tinyBankC: TinyBank;
-    beforeEach(async () => {
-        signers = await hre.ethers.getSigners();
-        myTokenC = await hre.ethers.deployContract("MyToken", [
-            "MyToken",
-            "MT",
-            DECIMALS,
-            MINTING_AMOUNT,
-        ]);
-        tinyBankC = await hre.ethers.deployContract("TinyBank", [
-            await myTokenC.getAddress(),
-        ]);
-        await myTokenC.setManager(tinyBankC.getAddress());
-    });
+  let signers: HardhatEthersSigner[];
+  let myTokenC: MyToken;
+  let tinyBankC: TinyBank;
 
-    describe("Initialized state check", () => {
-        it("should return totalStaked 0", async () => {
-            expect(await tinyBankC.totalStaked()).equal(0);
-        });
-        it("should return staked 0 amount of singer0", async () => {
-            expect(await tinyBankC.staked(signers[0].address)).equal(0);
-        });
-    })
+  beforeEach(async () => {
+    signers = await hre.ethers.getSigners();
+    myTokenC = await hre.ethers.deployContract("MyToken", [
+      "MyToken",
+      "MT",
+      DECIMALS,
+      MINTING_AMOUNT,
+    ]);
 
-    describe("Staking", async () => {
-        it("should return staked amount", async () => {
-            const signer0 = signers[0];
-            const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
-            await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
-            await tinyBankC.stake(stakingAmount);
-            expect(await tinyBankC.staked(signer0.address)).equal(stakingAmount);
-            expect(await tinyBankC.totalStaked()).equal(stakingAmount);
-            expect(await myTokenC.balanceOf(tinyBankC)).equal(await tinyBankC.totalStaked());
-        });
-    });
+    const managers = signers.slice(0, 5).map((s) => s.address);
+    tinyBankC = await hre.ethers.deployContract("TinyBank", [
+      await myTokenC.getAddress(),
+      managers,
+    ]);
+    await myTokenC.setManager(await tinyBankC.getAddress());
+  });
 
-    describe("Withdraw", () => {
-        it("should return 0 staked after withdrawing total token", async () => {
-            const signer0 = signers[0];
-            const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
-            await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
-            await tinyBankC.stake(stakingAmount);
-            await tinyBankC.withdraw(stakingAmount);
-            expect(await tinyBankC.staked(signer0.address)).equal(0);
-        });
-    });
+  it("should revert when not all managers confirmed", async () => {
+    await tinyBankC.connect(signers[0]).confirm();
+    await tinyBankC.connect(signers[1]).confirm();
+    await expect(
+      tinyBankC.connect(signers[0]).setRewardPerBlock(123)
+    ).to.be.revertedWith("Not all confirmed yet");
+  });
 
-    describe("reward", () => {
-        it("should reward 1MT every block", async () => {
-            const singer0 = signers[0];
-            const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
-            await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
-            await tinyBankC.stake(stakingAmount);
+  it("should allow setRewardPerBlock after all managers confirm", async () => {
+    const newReward = hre.ethers.parseUnits("1234", DECIMALS);
+    for (let i = 0; i < 5; i++) {
+      await tinyBankC.connect(signers[i]).confirm();
+    }
+    await tinyBankC.connect(signers[0]).setRewardPerBlock(newReward);
+    const result = await tinyBankC.rewardPerBlock();
+    expect(result).to.equal(newReward);
+  });
 
-            const BLOCKS = 5n;
-            const transferAmount = hre.ethers.parseUnits("1", DECIMALS);
-            for (var i = 0; i < BLOCKS; i++) {
-                await myTokenC.transfer(transferAmount, singer0.address);
-            }
-
-            await tinyBankC.withdraw(stakingAmount);
-            expect(await myTokenC.balanceOf(singer0.address)).equal(
-                hre.ethers.parseUnits((BLOCKS + MINTING_AMOUNT + 1n).toString())
-            );
-        });
-        it("should revert when changing rewardPerBlock by hacker", async () => {
-            const hacker = signers[3];
-            const rewardToChange = hre.ethers.parseUnits("10000", DECIMALS);
-            await expect(tinyBankC.connect(hacker).setRewardPerBlock(rewardToChange))
-            .to.be.revertedWith("You are not authorized to manage this contract");
-        });
-    });
+  it("should revert if non-manager tries to confirm", async () => {
+    await expect(tinyBankC.connect(signers[5]).confirm()).to.be.revertedWith(
+      "You are not a manager"
+    );
+  });
 });
